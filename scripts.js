6ddd9f9f7b66acc4b7b1071d79d9ccaeba11c8ac
Stefan Dimitrov <stefan.r7.dimitrov@gmail.com>
2020-09-15 14:41:25 +0300

    main functionality done

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+// Calculate
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber,
+};
+
 let firstValue = 0;
 let operatorValue = '';
 let awaitingNextValue = false;
@@ -9,17 +18,21 @@ let awaitingNextValue = false;
 function sendNumberValue(number) {
     //console.log(number);
     //If display value is 0, replace it.
-    const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    if (awaitingNextValue) {
+        calculatorDisplay.textContent = number;
+        awaitingNextValue = false;
+    } else {
+        const displayValue = calculatorDisplay.textContent; calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
 }
 //{if (calculatorDisplay.textContent === '0'){calculatorDisplay.textContent = number;}else{calculatorDisplay.textContent = calculatorDisplay.textContent + number}}
-
-console.log(inputBtns);
+//console.log(inputBtns);
 
 // Run on 'decimal' click
 function addDecimal() {
+    //Wnen NOT to add decimal
+    if (awaitingNextValue) return;
     //If no decimal, add one
-    //const displayValue = calculatorDisplay.textContent;
     if (!calculatorDisplay.textContent.includes('.')) {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
         //calculatorDisplay.textContent = calculatorDisplay.textContent + '.';
@@ -29,14 +42,32 @@ function addDecimal() {
 // Run on 'operator' click
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
+    // Updates operator without calculating
+    if (operatorValue && awaitingNextValue){
+        operatorValue = operator;
+        return;
+    } 
     //Assign firstValue if no value
     if (!firstValue) {
         firstValue = currentValue;
+    } else {
+        //console.log(firstValue, operatorValue, currentValue);
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        //console.log('calculation', calculation);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;
     }
     // Ready for next
+    awaitingNextValue = true;
     operatorValue = operator;
-    console.log('firstValue', firstValue);
-    console.log('operatorValue', operatorValue);
+}
+
+// Run on 'clear' click - Reset all values, display
+function Reset() {
+    firstValue = 0;
+    operatorValue = '';
+    awaitingNextValue = false;
+    calculatorDisplay.textContent = '0';
 }
 
 // Add Event Listners
@@ -51,13 +82,4 @@ inputBtns.forEach((inputBtn) => {
     }
 });
 
-// Run on 'clear' click - Reset all values, display
-function Reset() {
-    firstValue = 0;
-    operatorValue = '';
-    awaitingNextValue = false;
-    calculatorDisplay.textContent = '0';
-}
-
-//Event Listner
 clearBtn.addEventListener('click', Reset);
